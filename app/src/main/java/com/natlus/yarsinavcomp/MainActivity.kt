542528fdfbf933ec5666d137c31f64ae5aa17bf1
Sultan Achmad <sultangendut@gmail.com>
2020-10-14 10:45:39 +0700

    initial commit

---
+++ app/src/main/java/com/natlus/yarsinavcomp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.natlus.yarsinavcomp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
